8904abefeaaba172dfd922abe43060eb32778394
imlnr <bablureddy553@gmail.com>
2024-01-17 21:28:04 +0530

    final commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,51 +1,36 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addcollege_success } from '../Redux/action-types';
+import "./css/home.css";
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     const state = useSelector((state) => state);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     useEffect(() => {
-        dispatch(addcollege_success({
-            "id": 1,
-            "name": "VIT College",
-            "universities": "Oxford University",
-            "location": "Cityville, State",
-            "image": "image.com",
-            "college-email": "contacts@vit.in",
-            "college-phone": "+91454545454",
-            "courses": [
-                {
-                    "course-title": "Full Stack Web Development",
-                    "course-price": 250000,
-                    "description": "Description for the colleges",
-                    "level": "Begineer",
-                    "Duration": 260,
-                    "Rating": 3,
-                    "language": [
-                        "English",
-                        "Hindi"
-                    ],
-                    "skills": [
-                        "Html",
-                        "Css",
-                        "JavaScript",
-                        "React"
-                    ],
-                    "reviews": 26
-                }
-            ],
-            "image-gallery": ["image.com", "image2.com"]
-        }));
     }, [])
     // console.log(state);
-    return (
-        <div>
-            Home page
-            <pre>
-                {JSON.stringify(state, null, 2)}
-            </pre>
+    return (<div id='container-home'>
+        <div id='bg-img'>
+            <div id='full-100vh'>
+                <div className='div-content'>
+                    <p>WELCOME TO EDUSPRINT</p>
+                    <h1>Best Online Education <br />Expertise</h1>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia <br /> and Consonantia, there live the blind texts.</p>
+                    <div className='div-c-buttons'>
+                        <button onClick={()=> navigate('/')}>GET STARTED NOW!</button>
+                        <button onClick={()=> navigate('/course')}>VIEW COURSES</button>
+                    </div>
+                </div>
+            </div>
         </div>
+        <div id='learn-anything'>
+            <div className="inner-learn-anything">
+            </div>
+            
+        </div>
+    </div>
     )
 }
 
