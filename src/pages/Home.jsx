ffc2ac708d019cb5bd22c65590cbebdc0e236f16
imlnr <bablureddy553@gmail.com>
2024-01-18 03:07:18 +0530

    final commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,13 +28,13 @@ const Home = () => {
         </div>
         <div id='learn-anything'>
             <div className="inner-learn-anything">
-                <img  src="https://xero-staff.com/images/cvs.png" alt="" />
+                <img src="https://xero-staff.com/images/cvs.png" alt="" />
                 <div className='ben-details'>
                     <p>LEARN ANYTHING</p>
                     <h1>Benefits About Online <br /> Learning Expertise</h1>
                     <div className="ben-cards">
                         <div className='ben-card-item'>
-                            <span class="material-symbols-outlined">
+                            <span className="material-symbols-outlined gs">
                                 menu_book
                             </span>
                             <div className='ben-card-text'>
@@ -43,7 +43,7 @@ const Home = () => {
                             </div>
                         </div>
                         <div className='ben-card-item'>
-                            <span class="material-symbols-outlined">
+                            <span className="material-symbols-outlined gs">
                                 card_membership
                             </span>
                             <div className="ben-card-text">
@@ -52,7 +52,7 @@ const Home = () => {
                             </div>
                         </div>
                         <div className='ben-card-item'>
-                            <span class="material-symbols-outlined">
+                            <span className="material-symbols-outlined gs">
                                 engineering
                             </span>
                             <div className="ben-card-text">
@@ -65,9 +65,44 @@ const Home = () => {
             </div>
 
         </div>
-        <div className="counts">
+        <div className="ben-counts">
             <div className='inner-counts'>
-                
+                <div className="count-items">
+                    <span className="material-symbols-outlined">
+                        person_2
+                    </span>
+                    <div>
+                        <h1>3,000</h1>
+                        <p>SUCCESS STORIES</p>
+                    </div>
+                </div>
+                <div className="count-items">
+                    <span className="material-symbols-outlined">
+                        groups
+                    </span>
+                    <div>
+                        <h1>320</h1>
+                        <p>TRUSTED TUTORS</p>
+                    </div>
+                </div>
+                <div className="count-items">
+                    <span className="material-symbols-outlined">
+                        calendar_month
+                    </span>
+                    <div>
+                        <h1>1,000</h1>
+                        <p>SCHEDULES</p>
+                    </div>
+                </div>
+                <div className="count-items">
+                    <span className="material-symbols-outlined">
+                        menu_book
+                    </span>
+                    <div>
+                        <h1>587</h1>
+                        <p>COURSES</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
