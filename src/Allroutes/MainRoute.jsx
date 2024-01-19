323ceaa27f7e19b89534370b5a78eb65c7676fe2
imlnr <bablureddy553@gmail.com>
2024-01-19 22:22:33 +0530

    final commit

--- src/Allroutes/MainRoute.jsx
+++ src/Allroutes/MainRoute.jsx
@@ -15,38 +15,38 @@ import Search from "../pages/Search";
 import Payment from "../pages/Payment";
 
 const MainRoute = () => {
-  const state = useSelector((state) => state.isLoggedIn);
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about-us" element={<About />} />
-      <Route path="/contact-us" element={<Contact />} />
-      <Route
-        path="/cart"
-        element={
-          <PrivateRoute>
-            <Cart />
-          </PrivateRoute>
-        }
-      />
-      <Route path="*" element={<PageNotFound />} />
-      {!state ? <Route path="/login" element={<Login />} /> : null}
-      {!state ? <Route path="/sign-up" element={<Signup />} /> : null}
-      <Route path="/course/:id" element={<Courses />} />
-      <Route path="/search" element={<Search />} />
-      <Route
-        path="/profile"
-        element={
-          <PrivateRoute>
-            <Profile />
-          </PrivateRoute>
-        }
-      />
-      <Route path="/search" element={<Search />} />
-      <Route path="/payments" element={<Payment />} />
-      {/* <Route path */}
-    </Routes>
-  );
+    const state = useSelector((state) => state.isLoggedIn);
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about-us" element={<About />} />
+            <Route path="/contact-us" element={<Contact />} />
+            <Route
+                path="/cart"
+                element={
+                    <PrivateRoute>
+                        <Cart />
+                    </PrivateRoute>
+                }
+            />
+            <Route path="*" element={<PageNotFound />} />
+            {!state ? <Route path="/login" element={<Login />} /> : null}
+            {!state ? <Route path="/sign-up" element={<Signup />} /> : null}
+            <Route path="/course/:id" element={<Courses />} />
+            <Route path="/search" element={<Search />} />
+            <Route
+                path="/profile"
+                element={
+                    <PrivateRoute>
+                        <Profile />
+                    </PrivateRoute>
+                }
+            />
+            <Route path="/search" element={<Search />} />
+            <Route path="/payments" element={<Payment />} />
+            {/* <Route path */}
+        </Routes>
+    );
 };
 
 export default MainRoute;
