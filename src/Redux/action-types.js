0cc83a8295ae2963404813ff12ebf168ec7a3f44
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-01-21 17:57:26 +0530

    corrected url

--- src/Redux/action-types.js
+++ src/Redux/action-types.js
@@ -112,7 +112,7 @@ export const getCourses = (search) => {
         // let data = await axios.get(
         //   `http://localhost:8080/courses?courseTitle_like=${search}`
         // );
-        let data = await axios.get(`${url}courses?courseTitle_like=${search}`);
+        let data = await axios.get(`${url}/courses?courseTitle_like=${search}`);
         console.log(`${url}/courses?courseTitle_like=${search}`);
         dispatch({
           type: GET_COURSES_SUCCESS,
